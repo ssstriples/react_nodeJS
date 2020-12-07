5758658c6072824212956c3f86a68a3a58deb3bb
sunghunlee <lsh76032004@gmail.com>
2020-12-07 21:12:21 +0900

    [LECTURE_201207] NODEJS #7
    <Description>
     - NodeJS Basic #7 BodyParser, PostMan, Register

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended : true}));
+//application/json
+app.use(bodyParser.json());
+
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://lsh76032004:!dltmdgns44@cluster0.ivuiv.mongodb.net/<dbname>?retryWrites=true&w=majority', {
     useNewUrlParser : true, useUnifiedTopology : true, useCreateIndex : true, useFindAndModify : false
@@ -10,6 +18,20 @@ mongoose.connect('mongodb+srv://lsh76032004:!dltmdgns44@cluster0.ivuiv.mongodb.n
 
 app.get('/', (req, res) => {
   res.send('Hello World! 안녕하세요')
+})
+
+app.post('/register', (req, res) => {
+
+  // 회원 가입 할 때 필요한 정보들을 Client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body)
+  user.save((err, userInfo) => {
+    if(err) return res.json({success : false, err})
+    return res.status(200).json({
+      success : true
+    })
+  })
+
 })
 
 app.listen(port, () => {
