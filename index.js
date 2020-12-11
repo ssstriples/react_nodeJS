2b240c04004f93152cbc379f7a3178dcdf60a4ec
sunghunlee <lsh76032004@gmail.com>
2020-12-11 19:20:08 +0900

    [LECTURE_201211] Bcrypt 비밀번호 암호화 #10 		 로그인 기능 with Bcrypt #11 		 토큰 생성 with jsonwebtoken #12 <Description> 1. Bcrypt 비밀번호 암호화 #10 - Bcrypt 라이브러리를 이용하여 비밀 번호를 암호화하여 DB에 저장해줘야함. - 다운로드 : npm install bcrypt --save
    2. 로그인 기능 with Bcrypt #11
    - login route 만들기
    
    3. 토큰 생성 with jsonwebtoken #12
    - jsonwebtoken 라이브러리 다운로드
     : npm install jsonwebtoken --save
    - cookie-parser 다운로드
     : npm install cookie-parser --save

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const port = 5000
 const config = require('./config/key');
 
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const { User } = require("./models/User");
 
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended : true}));
 //application/json
 app.use(bodyParser.json());
+
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {
@@ -27,12 +30,55 @@ app.post('/register', (req, res) => {
   // 회원 가입 할 때 필요한 정보들을 Client에서 가져오면
   // 그것들을 데이터 베이스에 넣어준다.
   const user = new User(req.body)
+
   user.save((err, userInfo) => {
     if(err) return res.json({success : false, err})
     return res.status(200).json({
       success : true
     })
   })
+
+})
+
+app.post('/login', (req, res) => {
+
+  // 요청된 이메일을 DB에서 있는지 찾는다.
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if(!user) {
+      return res.json({
+        loginSuccess : false,
+        message : "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+
+    // 요청된 이메일이 DB에 있다면 비밀번호가 맞는 비밀번호인지 확인.
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if(!isMatch) {
+        return res.json({
+          loginSuccess : false,
+          message : "비밀번호가 틀렸습니다."
+        })
+      }
+
+      // 비밀번호가 같다면 토큰을 생성하기.
+      user.generateToken((err, user) => {
+        if(err) return res.status(400).send(err);
+
+        // 토큰을 저장한다. 쿠키 or 로컬 스토리지
+        res.cookie("x_auth", user.token)
+        .status(200)
+        .json({loginSuccess : true, userId : user._id})
+      })
+      
+    })
+  })
+
+
+  
+
+
+  
+
 
 })
 
