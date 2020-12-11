3cfebe8b6670174dd4c8b3a5f9e895d7c31be38a
sunghunlee <lsh76032004@gmail.com>
2020-12-11 18:23:58 +0900

    [LECTURE_201211] NODE MON #8 정보관리 #9
    <Description>
    1. NODE MON #8
    - 소스를 변화를 감지해서 자동으로 서버를 재시작하는 Tool
    - 다운로드 : npm install nodemon --save-dev
                 dev는 로컬에서만 사용하겠다는 의미(devDependencies추가됨)
    - script 추가 : "backend": "nodemon index.js"
    
    2. 정보 관리 #9
    - key.js에 mongoDB URI를 통해 DEV환경과 PRD환경에서 URI관리

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const config = require('./config/key');
+
 const bodyParser = require('body-parser');
 const { User } = require("./models/User");
 
@@ -11,13 +13,13 @@ app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://lsh76032004:!dltmdgns44@cluster0.ivuiv.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser : true, useUnifiedTopology : true, useCreateIndex : true, useFindAndModify : false
 }).then(() => console.log('MongoDB Connect...'))
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World! 안녕하세요')
+  res.send('Hello World! 안녕하세요~~ 이승훈')
 })
 
 app.post('/register', (req, res) => {
